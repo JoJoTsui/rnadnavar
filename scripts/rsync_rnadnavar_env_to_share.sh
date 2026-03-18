4d35ef26b09b90164b51de85cc952ce2d7fbc334
Joey Xu <xuzy1992.joey@gmail.com>
2026-03-18 20:34:41 +0800

    [chore]: add the rsync rnadnavar env to share script

---
+++ scripts/rsync_rnadnavar_env_to_share.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+# nf-core configs
+rsync -avP /t9k/mnt/hdd/work/Vax/pipeline/configs/ /t9k/mnt/WorkSpace/data/ngs/xuzhenyu/pipeline/configs/
+# rnadnavar related databases
+rsync -avP ~/joey/bio_db/ ~/WorkSpace/data/ngs/xuzhenyu/bio_db/
+# deepvariant related binaries & models
+rsync -avP /t9k/mnt/hdd/work/Vax/deepvariant/DeepVariant-1.9.0 /t9k/mnt/WorkSpace/data/ngs/xuzhenyu/dv/
+rsync -avP /t9k/mnt/hdd/work/Vax/deepvariant/models /t9k/mnt/WorkSpace/data/ngs/xuzhenyu/dv/
+# rnadnavar test dataset
+rsync -avP /t9k/mnt/hdd/work/Vax/sequencing/aim_exp/rdv_test/C008801/input/ /t9k/mnt/WorkSpace/data/ngs/xuzhenyu/work/rnadnavar_test/C008801/input/
+# rnadnavar pipeline
+rsync -avP /t9k/mnt/hdd/work/Vax/pipeline/rnadnavar/ /t9k/mnt/WorkSpace/data/ngs/xuzhenyu/pipeline/rnadnavar/
